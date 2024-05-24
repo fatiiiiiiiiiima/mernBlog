db0be1f4045e626d6f68fbaccf55620f6e1781f4
fatiiiiiiiiiima <160717230+fatiiiiiiiiiima@users.noreply.github.com>
2024-05-24 14:01:59 +0500

    init commit

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+export default function Header(){
+    return(
+        <header>
+      <a href="" className="logo"My Blog></a>
+      <nav>
+        <a href="">Login</a><br></br>
+        <a href="">register</a>
+      </nav>
+    </header>
+
+    );
+}
---
+++ src/Post.js
@@ -0,0 +1,18 @@
+export default function Post(){
+    return(
+        <div className="post">
+        <div className="image">
+        <img src="https://techcrunch.com/wp-content/uploads/2024/05/GettyImages-2153584882.jpg" alt=""/>
+        </div>
+        
+      <div className="texts">
+      <h2>The biggest French startups in 2024 according to the French government</h2>
+        <p className="info">
+          <a className="author">fatima suleman</a>
+          <time>2024-24-05 11:32</time>
+        </p>
+        <p className="summary">There are many ways countries can go about supporting their tech industries. In France it involves picking a yearly cohort of 120 high-potential startups — the French Tech 120 — including 40 private companies deemed as most promising, dubbed the Next40.</p>
+      </div> 
+      </div> 
+    );
+}
